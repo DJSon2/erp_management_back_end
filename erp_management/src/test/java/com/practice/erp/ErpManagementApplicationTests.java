5705cf905ccc3910f1c56a50fea41b527f6a4f43
DONGJIN\손동진 <sdj1st11@gmail.com>
2023-06-23 16:21:38 +0900

    2023-06-23 starter 추가

---
+++ erp_management/src/test/java/com/practice/erp/ErpManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
